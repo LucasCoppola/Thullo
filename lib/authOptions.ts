106696b591b918638b1e4191ebd3bac6ae3f8616
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-08-12 12:19:56 -0300

    work around for userId

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -3,6 +3,12 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import GoogleProvider from 'next-auth/providers/google'
 import prisma from '@/lib/prisma'
 
+declare module 'next-auth' {
+	interface Session {
+		userId: string
+	}
+}
+
 export const authOptions: NextAuthOptions = {
 	adapter: PrismaAdapter(prisma),
 	providers: [
@@ -11,5 +17,11 @@ export const authOptions: NextAuthOptions = {
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
 		})
 	],
+	callbacks: {
+		async session({ session, user }) {
+			session.userId = user.id
+			return Promise.resolve(session)
+		}
+	},
 	secret: process.env.NEXTAUTH_SECRET as string
 }
