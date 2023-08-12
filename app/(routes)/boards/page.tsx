523b82cf1d9479f57217365f1610543210832aeb
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-08-12 12:20:14 -0300

    layout for routes

---
+++ app/(routes)/boards/page.tsx
@@ -0,0 +1,8 @@
+export default function Boards() {
+	return (
+		<>
+			<h1>All Boards</h1>
+			<button>Add</button>
+		</>
+	)
+}
---
+++ app/(routes)/layout.tsx
@@ -0,0 +1,19 @@
+import Nav from '@/components/shared/nav'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({ weight: '400', subsets: ['latin'] })
+
+export default function RootLayout({
+	children
+}: {
+	children: React.ReactNode
+}) {
+	return (
+		<html lang="en">
+			<body className={poppins.className}>
+				<Nav />
+				{children}
+			</body>
+		</html>
+	)
+}
