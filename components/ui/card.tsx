77f81e0fd8cfc3cf1a8833d2ce0f6e3e4cf508fb
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-08-15 12:42:47 -0300

    display amount of members

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -3,10 +3,13 @@ import Image from 'next/image'
 const avatars = [
 	{ img: 'https://github.com/shadcn.png', name: 'shadcn' },
 	{ img: 'https://github.com/shadcn.png', name: 'shadcn' },
+	{ img: 'https://github.com/shadcn.png', name: 'shadcn' },
 	{ img: 'https://github.com/shadcn.png', name: 'shadcn' }
 ]
 
 export default function Card() {
+	const remainingAvatars = avatars.length - 3
+
 	return (
 		<div className="bg-white border rounded-xl shadow w-64 h-auto">
 			<Image
@@ -21,8 +24,8 @@ export default function Card() {
 				<h5 className="text-lg mb-3 font-medium tracking-tight text-gray-900">
 					Simple Project Board
 				</h5>
-				<div className="flex space-x-2 flex-row">
-					{avatars.map(({ img, name }, i) => (
+				<div className="flex space-x-3 flex-row items-center">
+					{avatars.slice(0, 3).map(({ img, name }, i) => (
 						<img
 							key={i}
 							title={name}
@@ -31,6 +34,12 @@ export default function Card() {
 							alt={`${name} avatar`}
 						/>
 					))}
+					{remainingAvatars > 0 && (
+						<span className="text-gray-500 text-sm tracking-tight">
+							+{remainingAvatars}{' '}
+							{remainingAvatars === 1 ? 'other' : 'others'}
+						</span>
+					)}
 				</div>
 			</div>
 		</div>
