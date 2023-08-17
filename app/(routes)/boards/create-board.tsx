c2c43b0f72d15af9a824503cbc18b8a8e0ad2da7
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-08-17 14:42:22 -0300

    fetched unsplash images & display them in the tab

--- app/(routes)/boards/create-board.tsx
+++ app/(routes)/boards/create-board.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { useState } from 'react'
+import axios from 'axios'
 import Image from 'next/image'
+import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import {
 	Dialog,
 	DialogContent,
@@ -11,19 +13,31 @@ import {
 	DialogTrigger
 } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
-import { Add } from '@/components/ui/icons'
-import axios from 'axios'
-import { useQuery } from '@tanstack/react-query'
+import { Add, Search } from '@/components/ui/icons'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import Link from 'next/link'
+
+type ImageData = {
+	id: string
+	alt: string
+	urls: { small: string }
+	user: { id: string; username: string; links: { html: string } }
+}
 
 export default function CreateBoard() {
 	const [isHovered, setIsHovered] = useState(false)
+	const [image, setImage] = useState('')
 
 	const { isLoading, error, data } = useQuery(['boards'], async () => {
-		const response = await axios.get(
-			`https://api.unsplash.com/photos/?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}`
+		const randomImages = await axios.get(
+			`https://api.unsplash.com/photos/?per_page=20&client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}`
 		)
-		const data = await response.data
-		return data
+		const searchImages = await axios.get(
+			`https://api.unsplash.com/search/photos?page=1&query=${image}&client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}`
+		)
+		const randomImage: ImageData[] = await randomImages.data
+		const searchImage: ImageData[] = await searchImages.data.results
+		return { randomImage, searchImage }
 	})
 
 	return (
@@ -52,6 +66,8 @@ export default function CreateBoard() {
 							<CoverImageModal
 								isHovered={isHovered}
 								data={data}
+								image={image}
+								setImage={setImage}
 							/>
 						</div>
 						<input
@@ -110,10 +126,14 @@ export default function CreateBoard() {
 
 export function CoverImageModal({
 	isHovered,
-	data
+	data,
+	image,
+	setImage
 }: {
 	isHovered: boolean
-	data: any
+	data: { searchImage: ImageData[]; randomImage: ImageData[] } | undefined
+	image: string
+	setImage: (val: string) => void
 }) {
 	return (
 		<Dialog>
@@ -124,12 +144,64 @@ export function CoverImageModal({
 							? 'opacity-100 visibility-visible transition-opacity duration-300 ease-in-out'
 							: 'opacity-0 visibility-hidden transition-opacity duration-300 ease-in-out'
 					}`}
-					onClick={() => console.log(data)}
+					// onClick={() => console.log(data?)}
 				>
 					Change cover
 				</span>
 			</DialogTrigger>
-			<DialogContent></DialogContent>
+			<DialogContent>
+				<Tabs defaultValue="colors" className="w-[400px]">
+					<TabsList>
+						<TabsTrigger value="colors">Colors</TabsTrigger>
+						<TabsTrigger value="unsplash">Unsplash</TabsTrigger>
+					</TabsList>
+					<TabsContent value="colors">
+						Make changes to your account here.
+					</TabsContent>
+					<TabsContent
+						value="unsplash"
+						className="w-[29rem] overflow-y-scroll md:overflow-y-auto max-h-72"
+					>
+						<div className="relative mt-1">
+							<input
+								className="w-full py-2 pl-10 pr-10 text-gray-600 text-sm bg-white border rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+								placeholder="Search for an image..."
+								value={image}
+								onChange={(e) => setImage(e.target.value)}
+							/>
+							<span className="absolute inset-y-0 left-0 flex items-center pl-3">
+								<Search />
+							</span>
+						</div>
+						<div className="grid grid-cols-3 gap-3 mt-4">
+							{data?.randomImage.map(
+								({ id, alt, urls, user }: ImageData) => (
+									<div key={id}>
+										<Image
+											id={id}
+											alt={alt}
+											src={urls.small}
+											width={200}
+											height={50}
+											className="w-full h-16 rounded-sm object-cover"
+										/>
+										<div className="text-xs text-muted-foreground mt-1">
+											<span>by </span>
+											<Link
+												id={user.id}
+												href={user.links.html}
+												className="underline hover:text-red-500"
+											>
+												{user.username}
+											</Link>
+										</div>
+									</div>
+								)
+							)}
+						</div>
+					</TabsContent>
+				</Tabs>
+			</DialogContent>
 		</Dialog>
 	)
 }
