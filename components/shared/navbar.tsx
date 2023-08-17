e70ba6e790fc96a267abc5278544b911b98c91ce
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-08-17 13:32:50 -0300

    moved search icon

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -3,6 +3,7 @@ import UserDropdown from './user-dropdown'
 import Image from 'next/image'
 import { Button } from '../ui/button'
 import Link from 'next/link'
+import { Search } from '../ui/icons'
 
 export default function Navbar({ session }: { session: Session | null }) {
 	return (
@@ -30,19 +31,7 @@ export default function Navbar({ session }: { session: Session | null }) {
 									placeholder="Search..."
 								/>
 								<span className="absolute inset-y-0 left-0 flex items-center pl-3">
-									<svg
-										className="w-5 h-5 text-gray-400"
-										viewBox="0 0 24 24"
-										fill="none"
-									>
-										<path
-											d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-											stroke="currentColor"
-											strokeWidth="2"
-											strokeLinecap="round"
-											strokeLinejoin="round"
-										/>
-									</svg>
+									<Search />
 								</span>
 							</div>
 							<UserDropdown session={session} />
--- components/ui/icons/index.ts
+++ components/ui/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as Google } from './google'
 export { default as LoadingCircle } from './loading-circle'
 export { default as Add } from './add'
+export { default as Search } from './search'
