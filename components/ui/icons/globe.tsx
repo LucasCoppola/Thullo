e6f48c7159b39e4a414607ac014a7e6fb8c5cd9d
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-08-21 19:34:16 -0300

    color property

--- components/ui/icons/globe.tsx
+++ components/ui/icons/globe.tsx
@@ -1,5 +1,11 @@
-export default function Globe({ className }: { className?: string }) {
-	const gray = '#374151'
+export default function Globe({
+	className,
+	color
+}: {
+	className?: string
+	color?: string
+}) {
+	const gray700 = '#374151'
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -12,23 +18,23 @@ export default function Globe({ className }: { className?: string }) {
 				cy="32"
 				r="30"
 				fill="none"
-				stroke={gray}
-				strokeWidth="4"
+				stroke={color || gray700}
+				strokeWidth="3"
 			/>
 			<path
 				d="M53.85 47.85A27 27 0 0 1 24 57.8V56l3-3v-4l4-4v-3l4 4h5l2-2h8z"
-				fill={gray}
-				stroke={gray}
+				fill={color || gray700}
+				stroke={color || gray700}
 			/>
 			<path
 				d="M42 20.59v2.56L38.07 27H31l-5.36 5.26L31 37.51v5.06L27.44 39h-4.58L16 32.11V24.2L11.8 20h-4A27 27 0 0 1 32 5a26.55 26.55 0 0 1 7.06.94L36 9h-6v4l4 4h4.33z"
-				fill={gray}
-				stroke={gray}
+				fill={color || gray700}
+				stroke={color || gray700}
 			/>
 			<path
 				d="M32 60a28 28 0 1 1 28-28 28 28 0 0 1-28 28zm0-54a26 26 0 1 0 26 26A26 26 0 0 0 32 6z"
-				fill={gray}
-				stroke={gray}
+				fill={color || gray700}
+				stroke={color || gray700}
 			/>
 		</svg>
 	)
