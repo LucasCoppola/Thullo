2c5b0a5dca767eb7d9cd895dacb2080e5ad38b9c
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-08-22 12:39:13 -0300

    visibility dropdown ui

--- app/boards/[id]/board-header.tsx
+++ app/boards/[id]/board-header.tsx
@@ -14,18 +14,31 @@ import {
 } from '@/components/ui/dropdown-menu'
 
 const avatars = [
-	{ img: 'https://github.com/shadcn.png', name: 'shadcn' },
-	{ img: 'https://github.com/shadcn.png', name: 'shadcn' },
-	{ img: 'https://github.com/shadcn.png', name: 'shadcn' },
-	{ img: 'https://github.com/shadcn.png', name: 'shadcn' }
+	{
+		img: 'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+		name: 'Juan'
+	},
+	{
+		img: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
+		name: 'Esteban'
+	},
+	{
+		img: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+		name: 'John'
+	},
+	{
+		img: 'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+		name: 'Carla'
+	}
 ]
+
 export default function BoardHeader({
 	visibility
 }: {
 	visibility: $Enums.BoardVisibility
 }) {
 	return (
-		<div className="mt-8 mx-8 flex justify-between items-center">
+		<div className="mt-6 mx-8 flex justify-between items-center">
 			<div className="flex space-x-4 flex-row items-center">
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
@@ -47,24 +60,45 @@ export default function BoardHeader({
 						</Button>
 					</DropdownMenuTrigger>
 					<DropdownMenuContent align="start">
-						<DropdownMenuLabel>My Account</DropdownMenuLabel>
+						<DropdownMenuLabel>
+							<h2>Visibility</h2>
+							<span className="text-gray-500 text-xs">
+								Choose who can see this board.
+							</span>
+						</DropdownMenuLabel>
 						<DropdownMenuSeparator />
-						<DropdownMenuItem>Profile</DropdownMenuItem>
-						<DropdownMenuItem>Billing</DropdownMenuItem>
-						<DropdownMenuItem>Team</DropdownMenuItem>
-						<DropdownMenuItem>Subscription</DropdownMenuItem>
+						<DropdownMenuItem className="cursor-pointer flex-col items-start">
+							<div className="flex flex-row items-center">
+								<Globe className="h-4 w-4 mr-2" />
+								Public
+							</div>
+							<span className="text-gray-500 text-xs">
+								Anyone can see this board
+							</span>
+						</DropdownMenuItem>
+						<DropdownMenuItem className="cursor-pointer flex-col items-start">
+							<div className="flex flex-row items-center">
+								<Lock className="h-4 w-4 text-gray-500 mr-2" />
+								Private
+							</div>
+							<span className="text-gray-500 text-xs">
+								Only members can see this board
+							</span>
+						</DropdownMenuItem>
 					</DropdownMenuContent>
 				</DropdownMenu>
 
-				{avatars.slice(0, 3).map(({ img, name }, i) => (
-					<img
-						key={i}
-						src={img}
-						alt={`${name} avatar`}
-						title={name}
-						className="w-8 h-8 rounded-lg"
-					/>
-				))}
+				<div className="flex -space-x-1 overflow-hidden">
+					{avatars.map(({ img, name }, i) => (
+						<img
+							key={i}
+							src={img}
+							alt={`${name} avatar`}
+							title={name}
+							className="w-8 h-8 inline-block rounded-full ring-2 ring-white"
+						/>
+					))}
+				</div>
 				<Button size="icon" className="h-8 w-8" title="Add a member">
 					<Add className="h-[19px] w-[19px] text-gray-500" />
 				</Button>
