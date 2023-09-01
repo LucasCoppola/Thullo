9970f264e2e830b411e92d18df384fe84bcd1284
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-09-01 18:12:07 -0300

    editable title ui

--- app/boards/[id]/components/sheet-components/title.tsx
+++ app/boards/[id]/components/sheet-components/title.tsx
@@ -3,48 +3,65 @@ import { useEffect, useRef, useState } from 'react'
 export default function Title({ title }: { title: string }) {
 	const [isEditingTitle, setIsEditingTitle] = useState(false)
 	const [editedTitle, setEditedTitle] = useState(title)
-	const titleRef = useRef<HTMLDivElement>(null) // Reference to the title element
+	const titleRef = useRef<HTMLDivElement>(null)
+	const [titleWidth, setTitleWidth] = useState<number>(0)
+	console.log(titleWidth)
 
 	useEffect(() => {
-		// Add event listener to handle clicks outside of the title
-		const handleClickOutside = (event: any) => {
-			if (titleRef.current && !titleRef.current.contains(event.target)) {
-				// Clicked outside the title, so exit edit mode
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				titleRef.current &&
+				!titleRef.current.contains(event.target as Node)
+			) {
 				setIsEditingTitle(false)
 			}
 		}
 
-		// Attach the event listener
-		document.addEventListener('mousedown', handleClickOutside)
+		const updateTitleWidth = () => {
+			if (titleRef.current) {
+				const width = titleRef.current.offsetWidth
+				setTitleWidth(width / 2)
+			}
+		}
 
-		// Clean up the event listener on unmount
+		document.addEventListener('mousedown', handleClickOutside)
+		window.addEventListener('resize', updateTitleWidth)
+		updateTitleWidth()
 		return () => {
 			document.removeEventListener('mousedown', handleClickOutside)
+			window.removeEventListener('resize', updateTitleWidth)
 		}
-	}, [])
+	}, [editedTitle])
 
 	return (
 		<>
 			{isEditingTitle ? (
-				<div className="flex items-center w-32">
+				<div
+					className="flex items-center"
+					style={{ width: titleWidth + 'px' }}
+				>
 					<input
 						type="text"
 						value={editedTitle}
 						onChange={(e) => setEditedTitle(e.target.value)}
 						onBlur={() => setIsEditingTitle(false)}
 						autoFocus
-						className="border-b border-blue-500 px-2 py-1 w-32"
+						className="border-2 border-blue-200 px-2 py-0.5 text-xl font-semibold mb-1 rounded-sm focus:outline-none"
+						style={{ width: titleWidth + 'px' }}
 					/>
 				</div>
 			) : (
 				<div
-					className="flex items-center cursor-pointer"
-					onClick={() => setIsEditingTitle(true)} // Toggle edit mode when title is clicked
-					ref={titleRef} // Attach ref to the title element
+					role="button"
+					className="flex items-center"
+					onClick={() => setIsEditingTitle(true)}
+					ref={titleRef}
 				>
-					<h1 className="text-xl font-semibold">{editedTitle}</h1>
+					<h1 className="text-xl font-semibold px-2.5 py-1 hover:bg-gray-200 mb-1 rounded-sm">
+						{editedTitle}
+					</h1>
 				</div>
-			)}{' '}
+			)}
 		</>
 	)
 }
