6b2cceea4b7801a3821babb87591d78d92c50efe
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-09-14 19:50:02 -0300

    wierd width and height behaviour

--- components/card-components/attachment.tsx
+++ components/card-components/attachment.tsx
@@ -74,104 +74,104 @@ export default function AttachmentComponent({
 	)
 
 	return (
-		<>
-			<div className="flex flex-row w-full mb-3">
-				{isPDF ? (
-					<Link href={attachment.url} target="_blank">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 80 56"
-							className="rounded-lg mr-3"
-							width="80px"
-							height="56px"
+		<div className="flex flex-row w-full mb-3">
+			{isPDF ? (
+				<Link href={attachment.url} target="_blank">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 80 56"
+						className="rounded-lg"
+						width="80px"
+						height="56px"
+					>
+						<rect width="100%" height="100%" fill="#e0e0e0" />
+						<text
+							x="50%"
+							y="50%"
+							fontSize="20"
+							textAnchor="middle"
+							dy=".3em"
+							fill="#999"
 						>
-							<rect width="100%" height="100%" fill="#e0e0e0" />
-							<text
-								x="50%"
-								y="50%"
-								fontSize="20"
-								textAnchor="middle"
-								dy=".3em"
-								fill="#999"
-							>
-								{attachment.filename.slice(0, 2)}
-							</text>
-						</svg>
-					</Link>
-				) : (
-					<Link href={attachment.url} target="_blank">
+							{attachment.filename.slice(0, 2)}
+						</text>
+					</svg>
+				</Link>
+			) : (
+				<Link href={attachment.url} target="_blank">
+					<div style={{ width: '80px', height: '56px' }}>
 						<Image
 							src={attachment.url}
-							alt="unsplash random image"
-							width={400}
-							height={400}
-							className="w-20 h-14 object-cover rounded-lg mr-3"
+							alt="attachment preview"
+							className="h-14 object-cover rounded-lg"
+							width={500}
+							height={500}
 						/>
-					</Link>
-				)}
-				<div className="flex flex-col w-4/6">
-					<div className="flex flex-row">
-						<span className="text-[10px] text-gray-500">
-							Added{' '}
-							{new Date(attachment.uploadedAt).toLocaleDateString(
-								'en-us',
-								{
-									month: 'short',
-									day: 'numeric',
-									hour: 'numeric',
-									minute: 'numeric',
-									hour12: true
-								}
-							)}
-						</span>
-						<div className="ml-auto flex flex-row items-center">
-							<Download
-								className="h-3.5 w-3.5 text-blue-600"
-								role="button"
-								onClick={downloadFile}
-							/>
-
-							<AlertDialog>
-								<AlertDialogTrigger asChild>
-									<Trash
-										className="h-3.5 w-3.5 text-red-600 ml-2"
-										role="button"
-									/>
-								</AlertDialogTrigger>
-								<AlertDialogContent>
-									<AlertDialogHeader>
-										<AlertDialogTitle>
-											Are you absolutely sure?
-										</AlertDialogTitle>
-										<AlertDialogDescription>
-											This action cannot be undone. This
-											will permanently delete this
-											attachment from our servers.
-										</AlertDialogDescription>
-									</AlertDialogHeader>
-									<AlertDialogFooter>
-										<AlertDialogCancel>
-											Cancel
-										</AlertDialogCancel>
-										<AlertDialogAction
-											className="bg-red-500 hover:bg-red-600"
-											onClick={() => mutate()}
-										>
-											Delete
-										</AlertDialogAction>
-									</AlertDialogFooter>
-								</AlertDialogContent>
-							</AlertDialog>
-						</div>
 					</div>
-					<h3 className="text-xs text-gray-900 font-medium my-auto">
-						{attachment.filename}
-					</h3>
-					<span className="text-[10px] text-gray-500 mt-auto">
-						{user?.name ? `by ${user.name}` : null}
+				</Link>
+			)}
+			<div className="flex flex-col w-full ml-3">
+				<div className="flex flex-row">
+					<span className="text-[10px] text-gray-500">
+						Added{' '}
+						{new Date(attachment.uploadedAt).toLocaleDateString(
+							'en-us',
+							{
+								month: 'short',
+								day: 'numeric',
+								hour: 'numeric',
+								minute: 'numeric',
+								hour12: true
+							}
+						)}
 					</span>
+					<div className="ml-auto flex flex-row items-center">
+						<Download
+							className="h-3.5 w-3.5 text-blue-600"
+							role="button"
+							onClick={downloadFile}
+						/>
+
+						<AlertDialog>
+							<AlertDialogTrigger asChild>
+								<Trash
+									className="h-3.5 w-3.5 text-red-600 ml-2"
+									role="button"
+								/>
+							</AlertDialogTrigger>
+							<AlertDialogContent>
+								<AlertDialogHeader>
+									<AlertDialogTitle>
+										Are you absolutely sure?
+									</AlertDialogTitle>
+									<AlertDialogDescription>
+										This action cannot be undone. This will
+										permanently delete this attachment from
+										our servers.
+									</AlertDialogDescription>
+								</AlertDialogHeader>
+								<AlertDialogFooter>
+									<AlertDialogCancel>
+										Cancel
+									</AlertDialogCancel>
+									<AlertDialogAction
+										className="bg-red-500 hover:bg-red-600"
+										onClick={() => mutate()}
+									>
+										Delete
+									</AlertDialogAction>
+								</AlertDialogFooter>
+							</AlertDialogContent>
+						</AlertDialog>
+					</div>
 				</div>
+				<h3 className="text-xs text-gray-900 font-medium my-auto">
+					{attachment.filename}
+				</h3>
+				<span className="text-[10px] text-gray-500 mt-auto">
+					{user?.name ? `by ${user.name}` : null}
+				</span>
 			</div>
-		</>
+		</div>
 	)
 }
