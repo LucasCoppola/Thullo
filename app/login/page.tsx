36becb7bee5dc8ec9aeaef2bceddeec9efbb9b45
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-09-27 12:09:28 -0300

    redirect to boards after login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -20,7 +20,7 @@ export default function LoginPage() {
 					<Button
 						onClick={() => {
 							setClickedGoogle(true)
-							signIn('google', { callbackUrl: '/' })
+							signIn('google', { callbackUrl: '/boards' })
 						}}
 					>
 						{clickedGoogle ? (
