671cbc58d3372ac3ebc7e25c5b575bf2a17374fd
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-10-02 10:54:05 -0300

    made props optional

--- components/add-list-btn.tsx
+++ components/add-list-btn.tsx
@@ -11,30 +11,25 @@ export default function AddButtonComponent({
 	listId
 }: {
 	name: 'card' | 'list'
-	boardId: string
-	listId: string
+	boardId?: string
+	listId?: string
 }) {
 	const [createMode, setCreateMode] = useState(false)
 
 	return (
 		<>
 			{createMode && name === 'list' ? (
-				<AddList setCreateMode={setCreateMode} boardId={boardId} />
+				<AddList setCreateMode={setCreateMode} boardId={boardId!} />
 			) : createMode && name === 'card' ? (
-				<AddCard setCreateMode={setCreateMode} listId={listId} />
+				<AddCard setCreateMode={setCreateMode} listId={listId!} />
 			) : (
 				<button
 					className="mt-4 flex flex-row bg-blue-50 h-8 items-center px-2.5 py-1 rounded-lg hover:bg-blue-100"
 					style={{ minWidth: '243px' }}
 					onClick={() => setCreateMode(true)}
 				>
-					<span className="text-blue-400 text-sm">
-						Add another {name}
-					</span>
-					<Add
-						className="text-blue-300 ml-auto w-5 h-5"
-						strokeWidth={1.5}
-					/>
+					<span className="text-blue-400 text-sm">Add another {name}</span>
+					<Add className="text-blue-300 ml-auto w-5 h-5" strokeWidth={1.5} />
 				</button>
 			)}
 		</>
