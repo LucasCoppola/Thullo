a9be6ac7056b549cd6e9cecb19c3f9b7cae4caed
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-10-02 12:35:37 -0300

    dnd init

--- components/list-components/list.tsx
+++ components/list-components/list.tsx
@@ -7,8 +7,10 @@ import { findListById } from '@/app/server/listsOperations'
 import Cards from '../card-components/cards'
 import { useQuery } from '@tanstack/react-query'
 import DeleteList from './delete-list'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
-export default function List({
+export default function ListComponent({
 	listId,
 	boardMembers,
 	boardId,
@@ -23,9 +25,39 @@ export default function List({
 }) {
 	const { data: list } = useQuery(['list', listId], async () => await findListById({ listId }))
 
+	const { setNodeRef, attributes, listeners, transform, transition, isDragging } = useSortable({
+		id: listId,
+		data: {
+			type: 'list',
+			list
+		}
+	})
+
+	const style = {
+		transition,
+		transform: CSS.Transform.toString(transform)
+	}
+
+	if (isDragging) {
+		return (
+			<div
+				ref={setNodeRef}
+				style={{
+					minWidth: '243px',
+					...style
+				}}
+				className="mt-4 px-1.5 pb-1.5 bg-[#e2e8f0] opacity-50 rounded-lg border border-blue-400 border-dashed"
+			></div>
+		)
+	}
+
 	return (
-		<div className="mt-4" style={{ minWidth: '243px' }}>
-			<div className="flex flex-row items-center justify-between pb-4">
+		<div
+			ref={setNodeRef}
+			className="mt-4 bg-[#f8f9fa] px-1.5 pb-1.5 rounded-lg"
+			style={{ minWidth: '243px', ...style }}
+		>
+			<div className="flex flex-row items-center justify-between pb-4" {...attributes} {...listeners}>
 				<EditableListTitle title={title} listId={listId} />
 				<DeleteList
 					listId={listId}
--- components/list-components/lists.tsx
+++ components/list-components/lists.tsx
@@ -2,25 +2,69 @@
 
 import { useQuery } from '@tanstack/react-query'
 import { getLists } from '@/app/server/listsOperations'
-import List from './list'
-import { getBoardMembers } from '@/app/server/membersOperations'
+import ListComponent from './list'
+import { SortableContext } from '@dnd-kit/sortable'
+import type { List, User } from '@prisma/client'
+import { useMemo, useState } from 'react'
+import { DndContext, DragOverlay, type DragStartEvent, type UniqueIdentifier } from '@dnd-kit/core'
+import AddButtonComponent from '../add-list-btn'
+import { createPortal } from 'react-dom'
 
-export default function Lists({ boardId, boardAuthorId }: { boardId: string; boardAuthorId: string }) {
+export default function Lists({
+	boardId,
+	boardMembers,
+	boardAuthorId
+}: {
+	boardId: string
+	boardMembers: User[]
+	boardAuthorId: string
+}) {
 	const { data: lists } = useQuery(['lists', boardId], async () => await getLists({ boardId }))
-	const { data: members } = useQuery(['board-members', boardId], async () => await getBoardMembers({ boardId }))
+	const [activeColumn, setActiveColumn] = useState<List | null>(null)
+
+	const listsId = useMemo(() => {
+		if (!lists) return []
+		return lists.map((list) => list.id) as (UniqueIdentifier | { id: UniqueIdentifier })[]
+	}, [lists])
+
+	function onDragStart(e: DragStartEvent) {
+		if (e.active.data.current?.type === 'list') {
+			setActiveColumn(e.active.data.current.list)
+		}
+	}
 
 	return (
-		<>
-			{lists?.map(({ id, title }) => (
-				<List
-					key={id}
-					listId={id}
-					title={title}
-					boardMembers={members}
-					boardId={boardId}
-					boardAuthorId={boardAuthorId}
-				/>
-			))}
-		</>
+		<DndContext onDragStart={onDragStart}>
+			<div className="flex flex-row gap-8">
+				<SortableContext items={listsId}>
+					{lists?.map(({ id, title }) => (
+						<ListComponent
+							key={id}
+							listId={id}
+							title={title}
+							boardMembers={boardMembers}
+							boardId={boardId}
+							boardAuthorId={boardAuthorId}
+						/>
+					))}
+				</SortableContext>
+				<AddButtonComponent name="list" boardId={boardId} />
+			</div>
+
+			{createPortal(
+				<DragOverlay>
+					{activeColumn && (
+						<ListComponent
+							listId={activeColumn.id}
+							title={activeColumn.title}
+							boardMembers={boardMembers}
+							boardId={boardId}
+							boardAuthorId={boardAuthorId}
+						/>
+					)}
+				</DragOverlay>,
+				document.body
+			)}
+		</DndContext>
 	)
 }
