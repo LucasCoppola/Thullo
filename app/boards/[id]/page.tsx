dc27a4beef296756c9f71cbccba8befb508804a1
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-10-02 12:35:37 -0300

    dnd init

--- app/boards/[id]/page.tsx
+++ app/boards/[id]/page.tsx
@@ -6,7 +6,6 @@ import { authOptions } from '@/lib/authOptions'
 import { redirect } from 'next/navigation'
 import type { Board, User } from '@prisma/client'
 import BoardHeader from '@/components/board-header'
-import AddButtonComponent from '@/components/add-list-btn'
 import Lists from '@/components/list-components/lists'
 
 export default async function BoardPage({ params }: { params: { id: string } }) {
@@ -31,12 +30,8 @@ export default async function BoardPage({ params }: { params: { id: string } }) 
 				currUserId={session.userId}
 				members={members as User[]}
 			/>
-			<div className="w-full overflow-x-auto bg-[#fafbfe] px-2 rounded-lg mt-4">
-				<div className="flex flex-row gap-8">
-					<Lists boardId={board.id} boardMembers={members as User[]} boardAuthorId={author?.id} />
-
-					<AddButtonComponent name="list" boardId={board.id} listId={id} />
-				</div>
+			<div className="w-full overflow-x-auto px-2 rounded-lg mt-4">
+				<Lists boardAuthorId={board.authorId} boardId={board.id} boardMembers={members as User[]} />
 			</div>
 		</div>
 	)
