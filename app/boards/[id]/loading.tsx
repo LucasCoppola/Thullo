a5cac2ddebdcc28509223f9d1d0a4e2d1590f444
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-10-15 14:18:07 -0300

    updated loading elemnt color

--- app/boards/[id]/loading.tsx
+++ app/boards/[id]/loading.tsx
@@ -2,14 +2,14 @@ export default function LoadingBoardIdPage() {
 	return (
 		<div className="mx-8 animate-pulse">
 			<div className="mt-4 flex items-center justify-between">
-				<div className="h-8 w-72 rounded-md bg-gray-200"></div>
-				<div className="h-6 w-8 rounded-md bg-gray-200"></div>
+				<div className="h-8 w-72 rounded-md bg-gray-100"></div>
+				<div className="h-6 w-8 rounded-md bg-gray-100"></div>
 			</div>
 			<div className="mt-10 flex gap-8 px-2">
-				<div className="h-[75vw] w-[265px] rounded-md bg-gray-200"></div>
-				<div className="h-[75vw] w-[265px] rounded-md bg-gray-200"></div>
-				<div className="h-[75vw] w-[265px] rounded-md bg-gray-200"></div>
-				<div className="h-[75vw] w-[265px] rounded-md bg-gray-200"></div>
+				<div className="h-[75vw] w-[265px] rounded-md bg-gray-100"></div>
+				<div className="h-[75vw] w-[265px] rounded-md bg-gray-100"></div>
+				<div className="h-[75vw] w-[265px] rounded-md bg-gray-100"></div>
+				<div className="h-[75vw] w-[265px] rounded-md bg-gray-100"></div>
 			</div>
 		</div>
 	)
